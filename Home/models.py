0fb75dd2b80a189721be5049bf4f92c535f0ff4e
48845 <108906651+irAbs174@users.noreply.github.com>
2023-03-18 19:52:59 +0000

    Simple Commit

--- Home/models.py
+++ Home/models.py
@@ -2,14 +2,3 @@ from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 from django.urls import reverse
-
-
-class PageAdmin(models.Model):
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True, max_length=255)
-    content = models.TextField()
-    tag = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return f'{self.slug}, {self.title}, {self.author}'
